12f8607716cdbcbdfb687f5076a3e5542e485fa0
Dumble009 <daichi19990821@gmail.com>
2023-01-16 21:29:11 +0900

    initial commit

---
+++ libsources/test_lib.cpp
@@ -0,0 +1,9 @@
+﻿#include "test_lib.h"
+
+namespace testLib
+{
+    int add(int a, int b)
+    {
+        return a + b;
+    }
+}
---
+++ libsources/test_lib.h
@@ -0,0 +1,6 @@
+﻿#pragma once
+
+namespace testLib
+{
+    int add(int a, int b);
+}
