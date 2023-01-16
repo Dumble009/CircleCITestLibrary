29701cd4b4f25147a1fc54193444dd0dad57c327
Dumble009 <daichi19990821@gmail.com>
2023-01-16 21:47:58 +0900

    libファイルをビルドする処理を追加し、そのlibファイルを使用するテストケースを追加

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,10 @@
 ﻿// ----------STLのinclude----------
 #include <iostream> // 入出力
+#include "test_lib.h"
 
 int main(void)
 {
     std::cout << "Hello World!" << std::endl;
+    int a = 1, b = 2;
+    std::cout << "1 + 2 = " << testLib::add(a, b) << std::endl;
 }
